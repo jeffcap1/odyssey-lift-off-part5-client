6fb75e2e077dbe8f783fc847887fe1bc2be1de50
Jeff Capone <jcapone@goodwaygroup.com>
2022-01-11 16:26:14 -0500

    replaced deprecated track and module length with durationInSeconds. Used process.env to determine local or production server.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://jeffcap1-server-catstronauts.herokuapp.com/',
+  uri: process.env === 'production' ? 'https://jeffcap1-server-catstronauts.herokuapp.com/' : 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
