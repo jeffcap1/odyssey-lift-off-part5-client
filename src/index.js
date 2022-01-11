e84c7103c2cb9a378460f802b4f68d11a797373f
Jeff Capone <jcapone@goodwaygroup.com>
2022-01-11 15:53:41 -0500

    update to production server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://jeffcap1-server-catstronauts.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
